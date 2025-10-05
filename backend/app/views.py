df9535e1633bd15f5b57a8c52d5bd3fca6add178
Enrique Calderon <ksobrenat32@ks32.dev>
2025-10-05 11:22:03 -0600

    fix: Import TokenObtainPairSerializer for JWT authentication handling

--- backend/app/views.py
+++ backend/app/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from .models import Organization, Auditor, Audit, Measurement
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
