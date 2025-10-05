f3fcda8de7bb694f99af765024b3ab1b9a1706aa
Mariana-Esquivel <marianamesquivel@gmail.com>
2025-10-05 04:52:11 -0600

    Cambios pequeños

--- aeroscanner/src/components/register/register.component.ts
+++ aeroscanner/src/components/register/register.component.ts
@@ -1,11 +1,79 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+//import { AuthenticationService } from '../../_service/authentication.service';
+import { Subscription } from 'rxjs';
+import { Router, RouterModule } from '@angular/router';
+//import { User } from '../../_model/user';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { SwalMessages } from '../../shared/swal-messages';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  swal: SwalMessages = new SwalMessages(); // swal messages
+
+  registerForm = new FormGroup(
+    {
+     mail: new FormControl('', [Validators.required]),
+     name: new FormControl('', [Validators.required]),
+     password: new FormControl('', [Validators.required]),
+     surname: new FormControl('', [Validators.required]),
+     username: new FormControl('', [Validators.required])
+    },    
+  );
+
+  public showLoading: boolean;
+  private subscriptions: Subscription[] = [];
+
+  constructor(private router: Router) {
+    this.showLoading = false;
+  }
+
+  ngOnInit(): void {
+    //if (this.authenticationService.isUserLoggedIn()) {
+      this.router.navigateByUrl('');
+
+  }
+
+  public onRegister(): void {
+    /** 
+    var usuarioFormValue  = this.registerForm.value as User;
+    var usuario: User = new User();
+
+    usuario.mail = usuarioFormValue.mail;
+    usuario.name = usuarioFormValue.name;
+    usuario.surname = usuarioFormValue.surname;
+    usuario.password = usuarioFormValue.password;
+    usuario.rfc = usuarioFormValue.rfc;
+    usuario.username = usuarioFormValue.username;
+
+    this.showLoading = true;
+    this.subscriptions.push(
+      this.authenticationService.register(usuario).subscribe(
+        (response: {message: string}) => {
+          this.showLoading = false;
+          this.swal.successMessage("¡Tu cuenta ha sido creada exitosamente!");
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.swal.errorMessage(errorResponse.error.message);
+          this.showLoading = false;
+        }
+      )
+    );*/
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  get fg() {
+    return this.registerForm.controls;
+  }
 
 }
