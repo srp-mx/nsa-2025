93ee7f7a3dccf0f5d1626dca86367d46e1400266
arletpinacho <arlet.pinacho@ciencias.unam.mx>
2025-10-04 22:17:01 -0600

    login

---
+++ aeroscanner/src/components/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule, CommonModule, FontAwesomeModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  faCoffee = faCoffee;
+  
+  loginForm = new FormGroup(
+    {
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    }
+  )
+
+  constructor(private router: Router) { }
+
+  public onLogin(): void {
+    var loginFormValue = this.loginForm.value as { username: string, password: string };
+
+    //Mandamos al back para ver si es válido 
+  }
+
+  get fg() {
+    return this.loginForm.controls;
+  }
+}
