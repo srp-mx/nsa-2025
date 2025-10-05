5b8e5de8b0d80f52b1d83ab07a32e1d6528d6949
arletpinacho <arlet.pinacho@ciencias.unam.mx>
2025-10-05 12:17:13 -0600

    html home

--- aeroscanner/src/app/app.routes.ts
+++ aeroscanner/src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { MapComponent } from '../components/map/map.component';
 import { RegisterComponent } from '../components/register/register.component';
+import { HomeComponent } from '../components/home/home.component';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
         path: 'login',
         component: LoginComponent
     },
@@ -16,4 +21,7 @@ export const routes: Routes = [
         path: 'register',
         component: RegisterComponent
     }
+    ]
+  },
+  { path: '**', redirectTo: 'hoome', pathMatch: 'full' }
 ];
